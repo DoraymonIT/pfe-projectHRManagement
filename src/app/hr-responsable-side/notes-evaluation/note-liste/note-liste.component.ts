5c283e69da38460777aff9e237a7fc29c9582c14
DoraymonIT <53580861+DoraymonIT@users.noreply.github.com>
2020-05-19 21:05:08 +0000

    commit 2 this night

--- src/app/hr-responsable-side/notes-evaluation/note-liste/note-liste.component.ts
+++ src/app/hr-responsable-side/notes-evaluation/note-liste/note-liste.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NoteServiceService } from 'src/app/controller/service/note-service.service';
+import { NoteGeneraleDeAnnee } from 'src/app/controller/model/note-generale-de-annee.model';
 
 @Component({
   selector: 'app-note-liste',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NOteListeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private noteService: NoteServiceService
+    ) { }
 
   ngOnInit(): void {
   }
-
+  get notes(): Array<NoteGeneraleDeAnnee> {
+    return this.noteService.notes;
+    }
 }
