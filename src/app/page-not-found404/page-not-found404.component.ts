4db46d37b38574e671dcbefdf597a95b6dcba28a
DoraymonIT <bendrimou@gmail.com>
2020-06-10 20:17:24 +0100

    4040-page error

--- src/app/page-not-found404/page-not-found404.component.ts
+++ src/app/page-not-found404/page-not-found404.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-not-found404',
   templateUrl: './page-not-found404.component.html',
-  styleUrls: ['./page-not-found404.component.css']
+  styleUrls: ['./page-not-found404.component.scss']
 })
 export class PageNotFound404Component implements OnInit {
 
